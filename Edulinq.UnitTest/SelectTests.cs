fb20ed4bb66febc57e3ec5ead1c713c979996c9d
Mike Clement <mike@softwareontheside.com>
2012-03-07 23:49:58 -0700

    use nuget package restore
    add SelectTests shell

---
+++ Edulinq.UnitTest/SelectTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Edulinq.UnitTest
+{
+    [TestFixture]
+    public class SelectTests
+    {
+    }
+}
